f00b8f88458a1ec4759ba2e521c9cae4584441b3
Delano Francis <deltel000@gmail.com>
2022-08-17 20:35:01 -0500

    Add allOf

--- src/test/CompletableFutureTest.java
+++ src/test/CompletableFutureTest.java
@@ -2,6 +2,9 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -34,6 +37,41 @@ public class CompletableFutureTest {
         String greeting = finalFuture.get();
 
         assertEquals("Hello World!", greeting);
+    }
+
+    @Test
+    void thenAccept() throws ExecutionException, InterruptedException {
+        List<String> stringList = new ArrayList<>();
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Hello");
+        CompletableFuture<Void> future = completableFuture.thenAccept(stringList::add);
+        future.get();
+
+        assertEquals(1, stringList.size());
+    }
+
+    @Test
+    void thenApply_thenAccept() throws ExecutionException, InterruptedException {
+        List<String> stringList = new ArrayList<>();
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Hello");
+        CompletableFuture<String> future = completableFuture.thenApply(s -> s + " World!");
+        CompletableFuture<Void> voidFuture = future.thenAccept(stringList::add);
+        voidFuture.get();
+
+        assertEquals(1, stringList.size());
+    }
+
+    @Test
+    void allOf() throws ExecutionException, InterruptedException {
+        List<String> stringList = Arrays.asList("Hello", "Good");
+        List<String> stringListCopy = new ArrayList<>();
+
+        CompletableFuture<?>[] futures = stringList.stream().map(str -> CompletableFuture.supplyAsync(() -> str + " Hello")
+                .thenApply(s -> s + " World!").thenAccept(stringListCopy::add)).toArray(CompletableFuture[]::new);
+
+        CompletableFuture<Void> finalFuture = CompletableFuture.allOf(futures);
+        finalFuture.get();
+
+        assertEquals(2, stringListCopy.size());
     }
 
 }
